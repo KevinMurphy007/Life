2ac949e4745937a5a284416e560c0d1a08084e46
K Murphy <kemurphy@canterburyschool.org>
2019-02-02 01:14:40 -0500

    Final working piece

--- conway.py
+++ conway.py
@@ -5,7 +5,7 @@ MY_DIR = os.path.dirname(os.path.realpath(__file__))
 MY_CONFIG_FILE = 'starting_board_10x10.csv'
 
 
-def display_board(board, gen):
+def master_board(location, board, gen):
     """
     take the board as a list of lists and display each entry.
     The board is a m x n rectangular list that contains
@@ -17,19 +17,147 @@ def display_board(board, gen):
 
     TODO: iterate thru the list of lists and display each position of the board
     """
+
     print(f'Displaying board for generation {gen}')
 
     # TODO: complete the display board code
 
-def count_num_neighbors(board, row, col):
+    with open(location) as body_file:
+            reader = csv.reader(body_file)
+            map = []
+            count = 0
+            for rows in reader:
+                print()
+                map.append([])
+                for cols in rows:
+                    if int(cols) == 1:
+                        print('*', end='')
+
+                    if int(cols) == 0:
+                        print('.', end='')
+
+                    map[count].append(int(cols))
+                count += 1
+            print()
+            reader = update_board(map)
+    #for gen number use (update board ) recursion becasue the gen num
+    #is the number off generations the board goes thru DUMMY
+
+
+def count_num_neighbors_pop(board):
     """
     take the row and col and find out how many neighbors the square has
     """
-    pass
+    rows = len(board)
+    cols = len(board[0])
+    neighbor_num=0
+    for j in range(rows):
+        if j == 0:#TOP ROPE
+            for i in range(cols):
 
+                if i == 0:#left, top corner
+                    if board[j][i+1] == 1:
+                        neighbor_num += 1
+                    if board[j+1][i]==1:
+                        neighbor_num += 1
+                    if board[j+1][i+1] == 1:
+                        neighbor_num += 1
+
+                if i+1 == cols:#right, top corner
+                    if board[j][i-1] == 1:
+                        neighbor_num += 1
+                    if board[j+1][i]==1:
+                        neighbor_num += 1
+                    if board[j+1][i-1] == 1:
+                        neighbor_num += 1
+
+                else:
+                    if i != 0:#top side
+                        if board[j][i-1] == 1:
+                            neighbor_num += 1
+                        if board[j+1][i]==1:
+                            neighbor_num += 1
+                        if board[j+1][i-1] == 1:
+                            neighbor_num += 1
+
+                        if board[j][i+1] == 1:
+                            neighbor_num += 1
+
+                        if board[j+1][i+1] == 1:
+                            neighbor_num += 1
+
+
+        if j + 1 == rows:#LOW GROUND
+            for i in range(cols):
+                if i == 0:#left, bottom corner
+                    if board[j][i+1] == 1:
+                        neighbor_num += 1
+                    if board[j-1][i]==1:
+                        neighbor_num += 1
+                    if board[j-1][i+1] == 1:
+                        neighbor_num += 1
+
+
+                if i+1 == cols:#right, bottom corner
+                    if board[j][i-1] == 1:
+                        neighbor_num += 1
+                    if board[j-1][i]==1:
+                        neighbor_num += 1
+                    if board[j-1][i-1] == 1:
+                        neighbor_num += 1
+
+                else:
+                    if i != 0:#bottom side
+                        if board[j][i-1] == 1:
+                            neighbor_num += 1
+                        if board[j-1][i]==1:
+                            neighbor_num += 1
+                        if board[j-1][i-1] == 1:
+                            neighbor_num += 1
+
+                        if board[j][i+1] == 1:
+                            neighbor_num += 1
+
+                        if board[j-1][i+1] == 1:
+                            neighbor_num += 1
+
+        if j + 1 != rows:#NORMAL
+            if j != 0:
+                for i in range(cols):
+                    if i != 0:
+                        if i +1 != cols:
+                            if board[j][i-1] == 1:
+                                neighbor_num += 1
+
+                            if board[j-1][i]==1:
+                                neighbor_num += 1
+
+                            if board[j-1][i-1] == 1:
+                                neighbor_num += 1
+
+                            if board[j+1][i]==1:
+                                neighbor_num += 1
+
+                            if board[j-1][i+1] == 1:
+                                neighbor_num += 1
+
+                            if board[j+1][i-1] == 1:
+                                neighbor_num += 1
+
+                            if board[j+1][i+1] == 1:
+                                neighbor_num += 1
+
+                            if board[j][i+1] == 1:
+                                neighbor_num += 1
+
+
+
+
+
+    return neighbor_num
     # TODO: calculate the number of neighbors that a square has
 
-    return -1
+
 
 def update_board(board):
     """
@@ -62,18 +190,21 @@ def update_board(board):
          map.  ie., a corner square has only 3 neighboring squares.  a
          Non-corner Edge square has 5 neighbors.
     """
-    pass
+    for row in board:
+        for col in row:
+            neighbor_num = count_num_neighbors_pop(board)
+            if col == 1:
+                if neighbor_num <= 1:
+                    col = 0
+                if neighbor_num >= 4:
+                    col = 0
+            if col == 0:
+                if neighbor_num == 3:
+                    col = 1
+    return board
+
 
     # TODO: modify the board to reflect the next generation
-
-def load_board(board, infile_name):
-    """
-    open the CSV file and read in the values.  These will
-    all be 1s or 0s and should populate the initial board.
-    """
-    pass
-
-    # TODO: load the csv file int the board
 
 def main():
     """
@@ -88,16 +219,13 @@ def main():
 
     infile_name = MY_DIR + '/' + MY_CONFIG_FILE
 
-    load_board(board, infile_name)
-
     generations_to_do = 10
 
     for i in range(generations_to_do-1):
-        display_board(board, i)
-        update_board(board)
+        master_board(infile_name, board, i)
 
     # do one final display to show end state
-    display_board(board, i+1)
+    master_board(infile_name, board, i+1)
 
 
 
